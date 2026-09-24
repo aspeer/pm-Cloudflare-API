82a9da4611383eba9f9c3cc699aab4255da2343d
Andrew Speer <andrew.speer@isolutions.com.au>
2026-09-24 14:46:56 +0930

    VERSION increment: 1.010

--- lib/Cloudflare/API/KV.pm
+++ lib/Cloudflare/API/KV.pm
@@ -28,7 +28,7 @@ use Cloudflare::API::Resource;
 
 #  Version information
 #
-$VERSION='1.009';
+$VERSION='1.010';
 
 
 #  All done. Positive return
@@ -201,9 +201,19 @@ Missing account context, invalid identifiers, bodies, values, or options cause P
 
 Andrew Speer <andrew.speer@isolutions.com.au>
 
-# LICENSE and COPYRIGHT #
+# LICENSE and COPYRIGHT
 
-Copyright (c) 2026 Andrew Speer. This software is free software under the same terms as Perl 5.
+This file is part of Cloudflare::API.
+
+This software is copyright (c) 2026 by Andrew Speer <andrew.speer@isolutions.com.au>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+Full license text is available at:
+
+<http://dev.perl.org/licenses/>
+
 
 =end markdown
 
